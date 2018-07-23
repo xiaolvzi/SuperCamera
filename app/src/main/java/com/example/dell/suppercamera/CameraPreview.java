22ba3664b09c176bb79f23bdb5d7403dce35647b
Joe Lv <joel@wicresoft.com>
2018-07-23 19:28:15 +0800

    finish the basiclly setting about camera

--- app/src/main/java/com/example/dell/suppercamera/CameraPreview.java
+++ app/src/main/java/com/example/dell/suppercamera/CameraPreview.java
@@ -2,22 +2,28 @@ package com.example.dell.suppercamera;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.nfc.Tag;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import java.io.IOException;
+import java.util.List;
+
+import static android.content.Context.WINDOW_SERVICE;
 
 /** A basic Camera preview class */
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     private static final String TAG = "CameraPreview";
     private SurfaceHolder mHolder;
     private Camera mCamera;
-
-    public CameraPreview(Context context, Camera camera) {
+    private Context mContext;
+    public CameraPreview(Context context) {
         super(context);
-        mCamera = camera;
-
+        this.mContext=context;
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
         mHolder = getHolder();
@@ -27,24 +33,30 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, now tell the camera where to draw the preview.
+        Log.e("lv","surfaceCreated");
+        int numberOfCameras = Camera.getNumberOfCameras();
+        if (numberOfCameras == 2) {
+            mCamera=Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
+
+        }else {
+            mCamera=Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+
+        }
+
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Cannot start preview", e);
         }
-    }
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // empty. Take care of releasing the Camera preview in your activity.
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
-
-        if (mHolder.getSurface() == null){
+        Log.e("lv","surfaceChanged");
+        if (mHolder.getSurface() == null) {
             // preview surface does not exist
             return;
         }
@@ -52,20 +64,55 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // stop preview before making changes
         try {
             mCamera.stopPreview();
-        } catch (Exception e){
+        } catch (Exception e) {
             // ignore: tried to stop a non-existent preview
         }
 
         // set preview size and make any resize, rotate or
         // reformatting changes here
+        Camera.Parameters parameters = mCamera.getParameters();
+
+        List<Camera.Size> allSizes = parameters.getSupportedPictureSizes();
+        Camera.Size size = allSizes.get(0); // get top size
+        for (int i = 0; i < allSizes.size(); i++) {
+            if (allSizes.get(i).width > size.width)
+                size = allSizes.get(i);
+        }
+
+        parameters.setPictureSize(size.width, size.height);
+
+        Display display = ((WindowManager)mContext.getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+
+        if(display.getRotation() == Surface.ROTATION_0) {
+            mCamera.setDisplayOrientation(90);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_270) {
+
+            mCamera.setDisplayOrientation(180);
+        }
+
+        mCamera.setParameters(parameters);
+
+
 
         // start preview with new settings
+
         try {
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
-
-        } catch (Exception e){
-            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Cannot start preview", e);
         }
     }
+
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+        Log.e("lv","surfaceDestroyed");
+        mCamera.stopPreview();
+        mCamera.release();
+    }
+
+
 }
--- app/src/main/java/com/example/dell/suppercamera/MainActivity.java
+++ app/src/main/java/com/example/dell/suppercamera/MainActivity.java
@@ -1,36 +1,105 @@
 package com.example.dell.suppercamera;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  Camera mCamera;
     private  CameraPreview mCameraPreview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("lv","onCreate");
         setContentView(R.layout.activity_main);
 
-        initView();
-
-    }
-
-    private void initView() {
-        FrameLayout preview = findViewById(R.id.camera_preview);
         initCamera();
-        preview.addView(mCameraPreview);
+
+        hideNavigationBar();
     }
+
+    private void hideNavigationBar() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
 
     private void initCamera() {
+
+        FrameLayout preview = findViewById(R.id.camera_preview);
+
         boolean b= CameraUtils.checkCameraHardware(this);
-        mCamera=CameraUtils.getCameraInstance();
-        mCameraPreview = new CameraPreview(this, mCamera);
+        if (b) {
+            mCameraPreview = new CameraPreview(this);
+        }else {
+            Toast.makeText(this,"there is no camera in your phone",Toast.LENGTH_SHORT).show();
+        }
+
+        preview.addView(mCameraPreview);
+        //Add camera operation bar under the preview
+        LinearLayout cameraOptionBar = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.camera_option_bar, null);
+        cameraOptionBar.setGravity(Gravity.BOTTOM);
+        preview.addView(cameraOptionBar);
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT > 19) {
+            updateUI();
+        }
+        Log.e("lv","onResume");
+    }
+    @SuppressLint("NewAPi")
+    public void updateUI() {
+        final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
+                }
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("lv","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("lv","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("lv","onDestroy");
+    }
 }
